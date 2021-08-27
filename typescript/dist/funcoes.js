350c2fd5e26ce62939f748339a7a0e370d612aff
JessyPS <89113356+JessyPS@users.noreply.github.com>
2021-08-27 08:44:11 -0300

    resolvendo

---
+++ typescript/dist/funcoes.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+function somar(num1, num2) {
+    return num1 + num2;
+}
+exports["default"] = { somar: somar };
---
+++ typescript/dist/teste.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var funcoes_1 = require("./funcoes");
+console.log(funcoes_1["default"].somar(1, 2));
